4a77eff14a15428992b65a88700b2873a26f20c3
oppsec <daniel.sdm.github@gmail.com>
2021-03-25 17:04:27 -0300

    ui: updated banner version

--- design/banner.py
+++ design/banner.py
@@ -3,4 +3,4 @@ from rich import print
 def get_banner():
     with open('design/banner.txt') as file:
         content = file.read()
-        print(f"[yellow]{content}[/]")
+        print(f"[yellow]{content} [/]")
