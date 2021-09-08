86dead18bafdd02d901ef96121ceedbe7cde0efc
knightmare <daniel.sdm.github@gmail.com>
2021-09-08 15:03:50 -0300

    fix: minor changes

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-from src.interface.ui import clear, banner
+from src.interface.ui import banner
 from src.juumla.main import start
 
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    clear()
     banner()
 
     parser: str = ArgumentParser()
-    parser.add_argument('-u', help='Target URL to run Juumla', required=True)
-    args: str   = parser.parse_args()
+    parser.add_argument('-u', help='Run Juumla on target', required=True)
+    args: str = parser.parse_args()
 
     start(args)
--- src/juumla/settings.py
+++ src/juumla/settings.py
@@ -5,6 +5,7 @@ def get_user_agent() -> str:
     """ Returns a user-agent from user-agents.txt file """
 
     filename: str = "src/juumla/user-agents.txt"
+    
     with open(filename) as file:
         user_agents: str = file.readlines()
         user_agents: str = user_agents[randint(0, len(user_agents) -1)]
@@ -20,7 +21,6 @@ headers = {
 
 props = {
     'verify': False,
-    'timeout': 20,
     'allow_redirects': True,
     'headers': headers
 }
