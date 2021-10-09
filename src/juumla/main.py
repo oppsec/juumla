c41754a63d34655e28431c6b4f1a716d4a308c54
oppsec <opp@nuke.africa>
2021-10-09 13:26:43 -0300

    new: create table with version, target and date

--- src/juumla/main.py
+++ src/juumla/main.py
@@ -1,16 +1,30 @@
 from requests import get, exceptions
-from rich import print
+from rich import box, print
+from rich.table import Table
 from urllib3 import disable_warnings
 
-from src.juumla.settings import props
+from src.juumla.settings import props, get_today_date
 from src.juumla.modules.version import get_version
 
 disable_warnings()
 
+
 def start(args) -> None:
-    " Check if host address is alive to start Juumla checker "
+    " Check if host address is alive to start Juumla "
+
+    # Generate information table
+    table = Table(title="", box=box.SQUARE)
+
+    table.add_column("Version", justify="center", style="yellow")
+    table.add_column("Target", justify="center", style="yellow")
+    table.add_column("Date", justify="center", style="yellow")
+
+    table.add_row("build: 0.1.3", f"{args.u}", f"{get_today_date}")
+
+    print(table)
 
     try:
+
         response: str = get(args.u, **props)
         status_code: str = response.status_code
         body: str = response.text
@@ -22,20 +36,20 @@ def start(args) -> None:
         return print(f"[red][-] Connection problems with {args.u} | {error} [/]")
 
     except exceptions.MissingSchema as error:
-        return print(f"[red][-] Invalid URL, try with: http(s)://{args.u}")
+        return print(f"[red][-] Invalid URL, try with: http(s)://{args.u} [/]")
 
     except exceptions.InvalidURL as error:
-        return print(f"[red][-] Invalid URL... | {error}")
+        return print(f"[red][-] Invalid URL... | {error} [/]")
 
 
 def detect_joomla(args, body) -> None:
-    " Run checker to detect if target is running Joomla CMS "
+    " Try to detect if target is actually running Joomla CMS "
 
-    print("[yellow][!] Checking if target is running Joomla... [/]")
+    print("\n[yellow][!] Checking if target is running Joomla... [/]")
 
     basic = "Joomla"
-    meta = """<meta name="generator" content="Joomla"""
-    csrf_token = """'X-CSRF-Token': Joomla.getOptions('csrf.token')"""
+    meta = '<meta name="generator" content="Joomla'
+    csrf_token = "'X-CSRF-Token': Joomla.getOptions('csrf.token')"
     script_type = "joomla-script-options"
 
     if basic or meta or csrf_token or script_type in body:
